303f47bc29473f82886665abd252ce3bbca3d6f7
douglasbrandao21 <douglasbrando5@gmail.com>
2020-01-16 12:12:46 -0300

    Mobile app  was created

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,185 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
+
+import api from "../services/api";
+
+export default function Main({ navigation }) {
+  const [currentRegion, setCurrentRegion] = useState(null);
+  const [devs, setDevs] = useState([]);
+  const [techs, setTechs] = useState("");
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync();
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+    loadInitialPosition();
+  }, []);
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
+
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get("/search", {
+      params: {
+        latitude,
+        longitude,
+        techs
+      }
+    });
+
+    console.log(response.data);
+
+    setDevs(response.data);
+  }
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <>
+      <MapView
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+        style={styles.map}
+      >
+        {devs.map(dev => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              latitude: dev.location.coordinates[1],
+              longitude: dev.location.coordinates[0]
+            }}
+          >
+            <Image
+              style={styles.avatar}
+              source={{
+                uri: dev.avatar_url
+              }}
+            />
+            <Callout
+              onPress={() => {
+                navigation.navigate("Profile", {
+                  github_username: dev.github_username
+                });
+              }}
+            >
+              <View style={styles.container}>
+                <Text style={styles.name}>{dev.name}</Text>
+                <Text style={styles.bio}>{dev.bio}</Text>
+                <Text style={styles.techs}>{dev.techs.join(", ")}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Buscar Devs por tecnologias"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
+        />
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
+          <MaterialIcons
+            name="my-location"
+            size={20}
+            color="#FFF"
+          ></MaterialIcons>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 50,
+    borderWidth: 4,
+    borderColor: "#FFF"
+  },
+  container: {
+    width: 260
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  bio: {
+    color: "#666",
+    marginTop: 5
+  },
+  techs: {
+    marginTop: 5
+  },
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+  textInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFFF",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8E4FFF",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
+  }
+});
